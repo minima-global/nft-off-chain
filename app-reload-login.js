6c4d270bf7ac3fa3d8be39459ea0844f5f8734d1
neilshah2000 <neilshahlimited@hotmail.com>
2022-07-06 17:00:04 +0100

    updated build script for mds login

--- app-reload-login.js
+++ app-reload-login.js
@@ -1,5 +1,8 @@
 const { exec } = require('child_process')
 const axios = require('axios')
+const https = require('https')
+const xpath = require('xpath')
+const DOMParser = require('xmldom').DOMParser
 
 const minimaRpcUrl = 'http://127.0.0.1:9002'
 
@@ -34,14 +37,23 @@ const callMinimaCommand = (command) => {
 
 const getInstalledMinidapps = () => {
     return callMinimaCommand('mds').then((response) => {
+        console.log('All minidapp instances', response.minidapps)
         return response.minidapps
+    })
+}
+
+const getMinidappPassword = () => {
+    return callMinimaCommand('mds').then((response) => {
+        const password = response.password
+        console.log('password', password)
+        return password
     })
 }
 
 const getInstalledNftMinidappInstances = () => {
     console.log('getInstalledNftMinidappInstances')
     return getInstalledMinidapps().then((minidapps) => {
-        return minidapps.filter((minidapp) => minidapp.name === 'nft-off-chain')
+        return minidapps.filter((minidapp) => minidapp.conf.name === 'nft-off-chain')
     })
 }
 
@@ -58,13 +70,45 @@ const uninstallAppInstance = (uid) => {
 const uninstallAllNftInstances = () => {
     console.log('uninstallAllNftInstances')
     return getInstalledNftMinidappInstances().then((instances) => {
+        console.log('NFT instances', instances)
         const promises = instances.map((instance) => uninstallAppInstance(instance.uid))
         return Promise.all(promises)
     })
 }
 
-const openMinidappInChrome = (uid) => {
-    exec(`chromium http://localhost:9003/${uid}/index.html`, (error, stdout, stderr) => {
+const getMiniHubHTML = (password) => {
+    const url = 'https://127.0.0.1:9003/login.html'
+    const httpsAgent = new https.Agent({ rejectUnauthorized: false })
+
+    const request = {
+        method: 'post',
+        url,
+        data: `password=${password}`,
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        httpsAgent,
+    }
+
+    return axios(request).then((res) => res.data)
+}
+
+const extractLastInstalledMinidappUrl = (miniHubHTML) => {
+    console.log('miniHubHTML', miniHubHTML)
+    const miniHubDoc = new DOMParser().parseFromString(miniHubHTML)
+
+    // gets link from the last li in the list of installed minidapps
+    const xPathString = 'string(//ul//*[last()][name()="li"]//a/@href)'
+
+    var minidappHref = xpath.select(xPathString, miniHubDoc)
+
+    console.log('minidappHref', minidappHref)
+    // take off first dot and add to https://127.0.0.1:9003
+    const fullUrl = 'https://127.0.0.1:9003' + minidappHref.substring(1)
+    return fullUrl
+}
+
+const openMinidappInChrome = (minidappFullUrl) => {
+    console.log('minidappFullUrl', minidappFullUrl)
+    exec(`chromium ${minidappFullUrl}`, (error, stdout, stderr) => {
         if (error) {
             console.log(`error: ${error.message}`)
             return
@@ -77,17 +121,9 @@ const openMinidappInChrome = (uid) => {
     })
 }
 
-/**
- * Unload old instances
- * Load new instance into mds
- * Open new instance in Chrome
- */
 uninstallAllNftInstances()
     .then(installMinidapp(newZipFilePath))
-    .then(getInstalledNftMinidappInstances)
-    .then((installed) => {
-        console.log('final installed minidapp', installed)
-        // there should only be 1, return the uid of the first
-        return installed[0].uid
-    })
+    .then(getMinidappPassword)
+    .then(getMiniHubHTML)
+    .then(extractLastInstalledMinidappUrl)
     .then(openMinidappInChrome)
